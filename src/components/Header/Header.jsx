33d8eb6a027f06544ffb48a9edb96b6ea57d616a
Melvin Wilson <13534603+NarrowGate@users.noreply.github.com>
2023-11-24 02:37:43 +0000

    Header state with useState

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,64 @@
 import styles from "./Header.module.css";
+import sites from "../../data/sites";
+import { useState } from "react";
 
 const Header = () => {
-  return <div className={styles.headerWrap}>Header</div>;
+  let [currentSite, setCurrentSite] = useState(null);
+  let [currentOperation, setCurrentOperation] = useState(null);
+  let [allOperations, setAllOperations] = useState(null);
+
+  let changeSite = (e) => {
+    setCurrentSite(e.target.value);
+    let site = sites.find((site) => {
+      return +site.id === +e.target.value;
+    });
+    setCurrentSite(site ? site : null);
+    setAllOperations(site && site.operations ? site.operations : null);
+  };
+
+  let changeOperation = (e) => {
+    setCurrentOperation(e.target.value);
+  };
+  return (
+    <div className={styles.headerWrap}>
+      <div className={styles.item}>
+        <select onChange={changeSite}>
+          <option>Select site</option>
+          {sites.map((site) => {
+            return (
+              <option key={site.id} value={site.id}>
+                {site.displayName}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className={styles.item}>
+        Site:
+        {currentSite ? (
+          <div>Site: {currentSite.displayName}</div>
+        ) : (
+          <div>Site not selected</div>
+        )}
+        Header
+      </div>
+
+      {allOperations ? (
+        <div className={styles.item}>
+          <select onClick={changeOperation}>
+            <option>Select Operations</option>
+            {allOperations.map((operation) => {
+              return (
+                <option key={operation.id} value={operation.id}>
+                  {operation.displayName}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/data/sites.js
@@ -0,0 +1,46 @@
+const sites = [
+  {
+    id: 1,
+    displayName: "Kerala",
+    operations: [
+      {
+        id: 1,
+        displayName: "Ernakulam",
+        app: ["typper", "rbber", "erser"],
+      },
+    ],
+  },
+  {
+    id: 2,
+    displayName: "Tamil Nadu",
+    operations: [
+      {
+        id: 1,
+        displayName: "Vellore",
+        app: ["typper", "shrper", "rbber", "erser"],
+      },
+      {
+        id: 2,
+        displayName: "Salem",
+        app: ["rbber", "erser"],
+      },
+    ],
+  },
+  {
+    id: 3,
+    displayName: "Maharashtra",
+    operations: [
+      {
+        id: 1,
+        displayName: "Mumbai",
+        app: ["typper", "shrper", "rbber", "erser"],
+      },
+      {
+        id: 2,
+        displayName: "Pune",
+        app: ["rbber", "erser"],
+      },
+    ],
+  },
+];
+export default sites;
